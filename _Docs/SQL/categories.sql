7b990bdf6c4f0a24c1a97b43670a6c357b0878ed
twong4 <62853188+twong4@users.noreply.github.com>
2021-08-02 22:24:39 -0400

    add documentation

---
+++ _Docs/SQL/categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO `categories` (name) VALUES ("No Category"),
+("Fruit"), ("Vegetable"), ("Legume"), ("Meat"), ("Fish"), ("Seafood"), ("Dessert"), ("Snack"), ("Bread"), ("Grain"),
+("Prepared Food"), ("Spice & Condiment"), ("Salad"), ("Sandwich"), ("Dairy Product"),
+("Alcohol"), ("Juice"), ("Tea"), ("Coffee"), ("Milk"), ("Soft Drink"), ("Energy Drink"),
+("Cleaning Supplies"), ("Cosmetic"), ("Medicine"), ("Supplement"), ("Personal Care"), ("Other");
---
+++ _Docs/SQL/locations.sql
@@ -0,0 +1,3 @@
+INSERT INTO `locations` (name) 
+VALUES ("Not Stored"), ("Refrigerator"), ("Freezer"), ("Mini-Fridge"), ("Pantry"), ("Counter"), 
+("Top Cupboard"), ("Bottom Cupboard"), ("Cellar"), ("Basement"), ("Washroom"), ("Bedroom"), ("Other");
